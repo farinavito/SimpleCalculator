280de4676654735e5fb2e923bef2d8ffd90cd8e2
Vito Farina <farinavito@gmail.com>
2019-11-17 14:30:47 +0100

    making code prettier

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,9 +1,7 @@
 while True:
-
-
     operator = input("Enter an operator [+, -, *, /]:  ")
-    num_1 = (input("Enter number 1: "))
-    num_2 = (input("Enter number 2: "))
+    num_1 = input("Enter number 1: ")
+    num_2 = input("Enter number 2: ")
 
     try:
         num_1 = int(num_1)
@@ -11,26 +9,20 @@ while True:
 
         if operator == "+":
             print(num_1 + num_2)
-
         elif operator == "-":
             print(num_1 - num_2)
-
         elif operator == "*":
             print(num_1 * num_2)
-
         elif operator == "/":
             print(num_1 / num_2)
-
         else:
             print("operation not supported.")
 
-
-    except (ValueError, TypeError,):
-        print("Your have to use a number.")
+    except (ValueError, TypeError):
+        print("You have to use a number.")
 
     except ZeroDivisionError:
-        print("Your can't divide a number by 0.")
-
+        print("You can't divide a number by 0.")
 
     more = input("Continue [y/n] ")
     if more.lower() == "n":
